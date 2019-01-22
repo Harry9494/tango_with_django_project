4cacb9b803694d76f5e6fd18453c9d95af8d0b62
Charalampos Konstantinidis <2421909k@student.gla.ac.uk>
2019-01-22 15:58:45 +0000

    fifth commit: rollback to satisfy chapter 3 test

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 
@@ -7,17 +7,19 @@ from rango.models import Page
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    category_list = Category.objects.order_by('-likes')[:5]
-    page_list = Page.objects.order_by('-views')[:5]
-    context_dict = {'categories': category_list, 'pages': page_list}
+    # category_list = Category.objects.order_by('-likes')[:5]
+    # page_list = Page.objects.order_by('-views')[:5]
+    # context_dict = {'categories': category_list, 'pages': page_list}
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/index.html', context=context_dict)
+    # return render(request, 'rango/index.html', context=context_dict)
+    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
 
 
 def about(request):
-    return render(request, 'rango/about.html', {})
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>View index page</a>")
+    # return render(request, 'rango/about.html', {})
 
 
 def show_category(request, category_name_slug):
